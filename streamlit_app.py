7fff8102a937dabcd9b8c9b9d56d4b8aa44d8cd5
Sandeep Anumula <sandeep.anumula@outlook.com>
2025-02-17 18:53:52 +0000

    adding-my-resume

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,18 @@
 import streamlit as st
 
-st.write('Hello world!')
+def main():
+    st.title("My Resume Viewer")
+    
+    # Button to display the resume
+    if st.button("Show My Resume"):
+        with open("my_resume.pdf", "rb") as file:
+            resume_bytes = file.read()
+            st.download_button(label="Download Resume", 
+                               data=resume_bytes, 
+                               file_name="My_Resume.pdf", 
+                               mime="application/pdf")
+            st.write("### Resume Preview:")
+            st.pdf("your_resume.pdf")
+
+if __name__ == "__main__":
+    main()
