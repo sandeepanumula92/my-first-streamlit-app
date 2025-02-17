cdd5ecfa19e47d92d4b6e81935c9c9ccba82fa99
Sandeep Anumula <sandeep.anumula@outlook.com>
2025-02-17 19:08:57 +0000

    updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,11 @@
 import streamlit as st
+import base64
+
+def display_pdf(file_path):
+    with open(file_path, "rb") as pdf_file:
+        base64_pdf = base64.b64encode(pdf_file.read()).decode("utf-8")
+        pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="900"></iframe>'
+        st.markdown(pdf_display, unsafe_allow_html=True)
 
 def main():
     st.title("My Resume Viewer")
@@ -12,8 +19,7 @@ def main():
                                file_name="My_Resume.pdf", 
                                mime="application/pdf")
             st.write("### Resume Preview:")
-            pdf_display = f'<iframe src="my_resume.pdf" width="700" height="900"></iframe>'
-            st.markdown(pdf_display, unsafe_allow_html=True)
+            display_pdf("my_resume.pdf")
 
 if __name__ == "__main__":
     main()
